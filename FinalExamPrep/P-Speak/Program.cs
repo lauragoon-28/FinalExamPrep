86403ec535b8f2a1daf24d67efa09fa392769f38
Laura Goon <Laura.goon@ou.edu>
2021-05-09 17:33:49 -0500

    completed a review of participation Speak, yayyyyy

---
+++ FinalExamPrep/P-Speak/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P_Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What type of animal would you like to know the sound of?(dog, cat, or giraffe)");
+            string answer = Console.ReadLine();
+
+            if (answer.ToLower() == "dog" || answer.ToLower() == "cat" || answer.ToLower() == "giraffe")
+            {
+                Console.WriteLine(Speak(answer));
+            }
+        }
+
+        static string Speak(string animal)
+        {
+            string sound = "";
+            if (animal == "dog")
+            {
+                sound = "Woof";
+                return sound;
+            }
+            else if (animal == "cat")
+            {
+                sound = "Meow";
+                return sound;
+            }
+            else if (animal == "Giraffe")
+            {
+                sound = "Pft";
+                return sound;
+            }
+            return "Invalid";
+        }
+
+    }
+}
