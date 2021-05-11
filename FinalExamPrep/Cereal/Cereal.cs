c6b292d094a8612215719e7f3cec5581433d1a7a
Laura Goon <Laura.goon@ou.edu>
2021-05-11 13:53:14 -0500

    finished all hw/participations to study for final.. I hope i do well hehe

---
+++ FinalExamPrep/Cereal/Cereal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cereal
+{
+    class Cereal
+    {
+        public string Manufacturer { get; set;}
+
+        public string Name { get; set; }
+
+        public double Calories { get; set; }
+
+        public double Cups { get; set; }
+
+        public Cereal()
+        {
+            Manufacturer = "";
+            Name = "";
+            Calories = -1;
+            Cups = -1;
+        }
+    }
+}
---
+++ FinalExamPrep/Cereal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Cereal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] productInfo = File.ReadAllLines("Cereal_Data.txt");
+
+            List<Cereal> cereals = new List<Cereal>(); 
+
+            for (int i = 0; i < productInfo.Length; i++)
+            {
+                string[] currentProduct = productInfo[i].Split('|');
+                Cereal c1 = new Cereal();
+                c1.Name = currentProduct[0];
+                c1.Manufacturer = currentProduct[1];
+                c1.Calories = Convert.ToDouble(currentProduct[2]);
+                c1.Cups = Convert.ToDouble(currentProduct[3]);
+                cereals.Add(c1);
+            }
+
+            Console.WriteLine("Cereals with a serving size of 1 cup or more:");
+            for (int i = 0; i < cereals.Count; i++)
+            {
+                if (cereals[i].Cups >= 1)
+                {
+                    Console.WriteLine($"     - Name:{cereals[i].Name}, Manufacturer:{cereals[i].Manufacturer}, Calories:{cereals[i].Calories}, Cups:{cereals[i].Cups}");
+                }
+            }
+         
+            Console.WriteLine("Cereals with 100 calories or less per serving");
+            for (int i = 0; i < cereals.Count; i++)
+            {
+                if (cereals[i].Calories <= 100)
+                {
+                    Console.WriteLine($"     - Name:{cereals[i].Name}, Manufacturer:{cereals[i].Manufacturer}, Calories:{cereals[i].Calories}, Cups:{cereals[i].Cups}");
+                }
+            }
+
+
+        }
+    }
+}
